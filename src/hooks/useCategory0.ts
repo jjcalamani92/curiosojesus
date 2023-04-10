25039497aab5fdb573594d8855173a58cc34f58c
Jesus Calamani <jesuscalamani92@gmail.com>
2023-04-09 21:45:16 -0400

    v

--- src/hooks/useCategory0.ts
+++ src/hooks/useCategory0.ts
@@ -7,7 +7,7 @@ export const useGetCategories0ByParentId = (parentId: string, categories: Catego
     queryKey: [ `get-categories-0`, parentId],
     // queryFn: () => getCategories(parentId),
     queryFn: async () => {
-      const data = await fetch(`https://criscms.vercel.app/api/portfolio/categories0/parentId/${parentId}`, {mode: 'no-cors', cache: 'force-cache'})
+      const data = await fetch(`https://criscms.vercel.app/api/portfolio/categories0/parentId/${parentId}`, {mode: 'no-cors', cache: 'no-store'})
       return data.json()
     },
     initialData: categories
